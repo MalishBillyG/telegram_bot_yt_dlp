f51aed47d9aa97f2a6c4d83b898c68256a5495cc
Billy <orlovskymike2@gmail.com>
2026-08-10 00:38:21 +0300

    создал определения источника для скачивания

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
 import asyncio
 import os
 
+from urllib.parse import urlparse
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+if not TOKEN:
+    raise ValueError("Переменная BOT_TOKEN не установлена")
+
+
 dp = Dispatcher()
+
+
+def get_platform(url):
+    domain = urlparse(url).netloc.lower()
+
+    if "youtube.com" in domain or "youtu.be" in domain:
+        return "youtube"
+
+    if "tiktok.com" in domain:
+        return "tiktok"
+
+    return None
 
 
 @dp.message(CommandStart())
@@ -20,8 +40,29 @@ async def start_handler(message: Message):
     )
 
 
+@dp.message()
+async def message_handler(message: Message):
+    url = message.text
+
+    platform = get_platform(url)
+
+    if platform == "youtube":
+        await message.answer("✅ Это YouTube")
+
+    elif platform == "tiktok":
+        await message.answer("✅ Это TikTok")
+
+    else:
+        await message.answer(
+            "❌ Я не смог определить платформу.\n\n"
+            "Отправь ссылку на YouTube или TikTok."
+        )
+
+
 async def main():
     bot = Bot(token=TOKEN)
+
+    print("🤖 Бот запущен!")
 
     await dp.start_polling(bot)
 
